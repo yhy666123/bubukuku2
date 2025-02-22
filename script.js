72e7c0fbf62630c2cf9ef50c620b7064bea8686e
yhy666123 <1206494171@qq.com>
2025-02-22 15:13:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+let yesButton = document.getElementById("yes");
+let noButton = document.getElementById("no");
+let questionText = document.getElementById("question");
+let mainImage = document.getElementById("mainImage");
+
+let clickCount = 0;  // 记录点击 No 的次数
+
+// No 按钮的文字变化
+// const noTexts = [
+//     "布布？你认真的吗",
+//     "走不走？！",
+//     "不许选这个！ ",
+//     "找倒霉？臭布布",
+//     "不行:(！"
+// ];
+// const noTexts = [
+// 	"嗝~真的吗？",
+//     "那好好吃早点！",
+//     "午饭！",
+//     "小骚烤！",
+//     "大寄腿！",
+//     "不需要:(！"
+// ];
+
+const noTexts = [
+	"不是！",
+    "不是！",
+    "不是！",
+    "不是！",
+    "不是！",
+    "不是！"
+];
+// No 按钮点击事件
+noButton.addEventListener("click", function () {
+    clickCount++;
+
+    // 让 Yes 变大，每次放大 2 倍
+    let yesSize = 1 + (clickCount * 1.2);
+    yesButton.style.transform = `scale(${yesSize})`;
+
+    // 挤压 No 按钮，每次右移 100px
+    let noOffset = clickCount * 50;
+    noButton.style.transform = `translateX(${noOffset}px)`;
+
+    // **新增：让图片和文字往上移动**
+    let moveUp = clickCount * 25; // 每次上移 20px
+    mainImage.style.transform = `translateY(-${moveUp}px)`;
+    questionText.style.transform = `translateY(-${moveUp}px)`;
+
+    // No 文案变化（前 5 次变化）
+    if (clickCount <= 5) {
+        noButton.innerText = noTexts[clickCount - 1];
+    }
+
+    // 图片变化（前 5 次变化）恋人
+ //    if (clickCount === 1) mainImage.src = "2.gif"; // 震惊
+ //    if (clickCount === 2) mainImage.src = "3.gif";   // 思考
+ //    if (clickCount === 3) mainImage.src = "4.gif";   // 生气
+ //    if (clickCount === 4) mainImage.src = "4.gif";  // 哭
+ //    if (clickCount === 5) mainImage.src = "5.gif";  // 之后一直是哭
+	// if (clickCount === 6) mainImage.src = "6.gif";  // 之后一直是哭
+	// if (clickCount === 7) mainImage.src = "8.gif";  // 之后一直是哭
+	// if (clickCount === 8) mainImage.src = "9.gif";  // 之后一直是哭
+	// if (clickCount	>= 8) mainImage.src = "9.gif";  // 之后一直是哭
+	//根本都是最可爱的小熊
+	// if (clickCount === 1) mainImage.src = "img/lovable/111.gif"; // 震惊
+	// if (clickCount === 2) mainImage.src = "img/lovable/112.gif";   // 思考
+	// if (clickCount === 3) mainImage.src = "img/lovable/114.gif";   // 生气
+	// if (clickCount === 4) mainImage.src = "img/lovable/115.gif";  // 哭
+	// if (clickCount === 5) mainImage.src = "img/lovable/116.gif";  // 之后一直是哭
+	// if (clickCount === 6) mainImage.src = "img/lovable/117.gif";  // 之后一直是哭
+	// if (clickCount === 7) mainImage.src = "img/lovable/117.gif";  // 之后一直是哭
+	// if (clickCount === 8) mainImage.src = "img/lovable/117.gif";  // 之后一直是哭
+	// if (clickCount	>= 8) mainImage.src = "img/lovable/117.gif";  // 之后一直是哭
+	
+	//出去玩
+	// if (clickCount === 1) mainImage.src = "img/lovable/211.gif"; // 震惊
+	// if (clickCount === 2) mainImage.src = "img/lovable/212.gif";   // 思考
+	// if (clickCount === 3) mainImage.src = "img/lovable/203.gif";   // 生气
+	// if (clickCount === 4) mainImage.src = "img/lovable/204.gif";  // 哭
+	// if (clickCount === 5) mainImage.src = "img/lovable/213.gif";  // 之后一直是哭
+	// if (clickCount === 6) mainImage.src = "img/lovable/214.gif";  // 之后一直是哭
+	// if (clickCount === 7) mainImage.src = "img/lovable/214.gif";  // 之后一直是哭
+	// if (clickCount === 8) mainImage.src = "img/lovable/207.gif";  // 之后一直是哭
+	// if (clickCount	>= 8) mainImage.src = "img/lovable/207.gif";  // 之后一直是哭
+	//减肥
+	// if(clickCount === 1) mainImage.src = "img/lovable/301.gif"; // 震惊
+	// if (clickCount === 2) mainImage.src = "img/lovable/302.gif";   // 思考
+	// if (clickCount === 3) mainImage.src = "img/lovable/303.gif";   // 生气
+	// if (clickCount === 4) mainImage.src = "img/lovable/304.gif";  // 哭
+	// if (clickCount === 5) mainImage.src = "img/lovable/305.gif";  // 之后一直是哭
+	// if (clickCount === 6) mainImage.src = "img/lovable/306.gif";  // 之后一直是哭
+	// if (clickCount === 7) mainImage.src = "img/lovable/306.gif";  // 之后一直是哭
+	// if (clickCount === 8) mainImage.src = "img/lovable/306.gif";  // 之后一直是哭
+	// if (clickCount	>= 8) mainImage.src = "img/lovable/306.gif";  // 之后一直是哭
+	//布布哭
+	if(clickCount === 1) mainImage.src = "img/lovable/402.gif"; // 震惊
+	if (clickCount === 2) mainImage.src = "img/lovable/403.gif";   // 思考
+	if (clickCount === 3) mainImage.src = "img/lovable/405.gif";   // 生气
+	if (clickCount === 4) mainImage.src = "img/lovable/406.gif";  // 哭
+	if (clickCount === 5) mainImage.src = "img/lovable/408.gif";  // 之后一直是哭
+	if (clickCount === 6) mainImage.src = "img/lovable/409.gif";  // 之后一直是哭
+	if (clickCount === 7) mainImage.src = "img/lovable/409.gif";  // 之后一直是哭
+	if (clickCount === 8) mainImage.src = "img/lovable/409.gif";  // 之后一直是哭
+	if (clickCount	>= 8) mainImage.src = "img/lovable/409.gif";  // 之后一直是哭
+});
+
+// Yes 按钮点击后，进入表白成功页面
+yesButton.addEventListener("click", function () {
+    document.body.innerHTML = `
+        <div class="yes-screen">
+            <h1 class="yes-text">!!!哭哭!! ..(｡•ˇ‸ˇ•｡)…</h1>
+            <img src="img/lovable/412.gif" alt="拥抱" class="yes-image">
+        </div>
+    `;
+
+    document.body.style.overflow = "hidden";
+});
